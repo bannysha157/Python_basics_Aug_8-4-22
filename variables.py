d18f23e7517e5332dd406d47a0e7afac44917542
bannysha <bannysha.manyapu@thundersoft.com>
2022-08-04 11:37:00 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+Variable names can be any length can have uppercase, lowercase (A to Z, a to z), the digit (0-9), and underscore character(_).
+name = "F"  
+Name = "R"  
+naMe = "E"  
+NAME = "E"  
+n_a_m_e ="D"  
+_name = "O"  
+name_ = "M"  
+_name_ = "B"  
+na56me = "B" 
+  
+print(name,Name,naMe,NAME,n_a_m_e, NAME, n_a_m_e, _name, name_,_name, na56me)
+
+o/p= F R E E D O M B B
+
+
+
+1. Assigning single value to multiple variables
+x=y=z=40;
+print(x)
+print(y)
+print(z)
+o/p=40
+40
+40
+2) Assigning multiple values to multiple variables:
+x,y,z=1,2,3;
+print(x)
+print(z)
+print(y)
+o/p=1,3,2
